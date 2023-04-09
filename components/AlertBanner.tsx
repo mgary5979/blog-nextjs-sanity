4c729bab4924582e5cfcc218bc22cf97c029ae17
mgary5979 <mgary5979@gmail.com>
2023-04-09 19:52:17 +0100

    changed code

--- components/AlertBanner.tsx
+++ components/AlertBanner.tsx
@@ -1,27 +1,27 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Container from 'components/BlogContainer'
 function Banner() {
-    return (
-    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+  return (
+    <div className="mb-10 flex flex-col justify-between px-10 py-5 font-bold lg:flex-row lg:space-x-5">
       <Container>
-     <div>
-     <h1 className="text-7xl">ChilledCat's Daily Blog</h1>
-     <h2 className="mt-5 md:mt-0">
-        Welcome to{" "}
-        <span className="underline decoration-4 decoration[#F7AB0A]">
-            Everyone's
-        </span>{" "}
-        favourite blog in the ChilledCat world ^.^
-     </h2>
-     </div>
+        <div>
+          <h1 className="text-7xl">ChilledCat's Daily Blog</h1>
+          <h2 className="mt-5 md:mt-0">
+            Welcome to{' '}
+            <span className="decoration[#F7AB0A] underline decoration-4">
+              Everyone's
+            </span>{' '}
+            favourite blog in the ChilledCat world ^.^
+          </h2>
+        </div>
 
-    <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
-     New innovative ways to become more happier | live more freely | become a better version of yourself!
-    </p>
-
-    </Container>
+        <p className="mt-5 max-w-sm text-gray-400 md:mt-2">
+          New innovative ways to become more happier | live more freely | become
+          a better version of yourself!
+        </p>
+      </Container>
     </div>
-    )
+  )
 }
 
 export default Banner
